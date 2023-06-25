2d8f9a4d6bd6fff627fe5e03116b8c52f4028e33
ahsanali7893 <ahsan3916902@gmail.com>
2023-06-25 13:16:11 +0500

    inital changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Hello Duniya</div>
+   <>
+   <header className='bg-gray-50'>
+    <div className='xl:container xl:mx-auto'>
+        <div>
+            <input type="text" />
+        </div>
+        <div>
+            <a>Design</a>
+        </div>
+        <div>
+            <div className="flex"></div>
+        </div>
+    </div>
+   </header>
+   </>
   )
 }
 
