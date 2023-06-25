1d30077677ddb2048d75f70ef0352ac53de47456
ahsanali7893 <ahsan3916902@gmail.com>
2023-06-25 13:20:47 +0500

    initial changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,15 +4,19 @@ const Header = () => {
   return (
    <>
    <header className='bg-gray-50'>
-    <div className='xl:container xl:mx-auto'>
+    <div className='xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3'>
         <div>
-            <input type="text" />
+            <input type="text" placeholder='search' />
         </div>
         <div>
             <a>Design</a>
         </div>
         <div>
-            <div className="flex"></div>
+            <div className="flex">
+                <a>Facebook</a>
+                <a>Twitter</a>
+                <a>Youtube</a>
+            </div>
         </div>
     </div>
    </header>
