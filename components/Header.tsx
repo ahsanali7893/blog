dac5b9e5de178b75fff2d50d4921de29345e4ade
ahsanali7893 <ahsan3916902@gmail.com>
2023-06-26 12:43:39 +0500

    initial changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,13 +6,13 @@ const Header = () => {
    <header className='bg-gray-50'>
     <div className='xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3'>
         <div className='md:flex-none w-96 order-2 sm:order-1'>
-            <input type="text" placeholder='search' />
+            <input type="text" className='mt-1 block w-60 px-3' placeholder='search' />
         </div>
         <div className='shrink w-80 sm:order-2'>   
             <a>Design</a>
         </div>
         <div className='w-96 order-3 justify-center flex'>
-            <div className="flex">
+            <div className="flex gap-6">
                 <a>Facebook</a>
                 <a>Twitter</a>
                 <a>Youtube</a>
